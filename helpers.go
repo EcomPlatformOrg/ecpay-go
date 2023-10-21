051f36756faef6bfa3a7395e327c8c8f18dded2e
Koopa <koopapapa@gmail.com>
2023-10-21 12:13:21 +0800

    add validate CheckMacValue

--- helpers.go
+++ helpers.go
@@ -3,6 +3,7 @@ package ecpayGo
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"errors"
 	"net/url"
 	"sort"
 	"strconv"
@@ -102,4 +103,25 @@ func generateCheckMacValue(values url.Values, hashKey string, hashIV string) str
 
 	// Step (6) 再轉大寫產生CheckMacValue
 	return strings.ToUpper(hashedValue)
+}
+
+// ValidateCheckMacValue validates the CheckMacValue from ECPay's response
+func ValidateCheckMacValue(responseValues url.Values, hashKey string, hashIV string) error {
+	// Extract the CheckMacValue from the response
+	receivedCheckMacValue := responseValues.Get("CheckMacValue")
+	if receivedCheckMacValue == "" {
+		return errors.New("CheckMacValue is missing from the response")
+	}
+
+	// Remove the CheckMacValue from the values as it's not included in the generation
+	responseValues.Del("CheckMacValue")
+
+	// Generate the CheckMacValue using the provided values
+	expectedCheckMacValue := generateCheckMacValue(responseValues, hashKey, hashIV)
+
+	if expectedCheckMacValue != receivedCheckMacValue {
+		return errors.New("CheckMacValue mismatch")
+	}
+
+	return nil
 }
