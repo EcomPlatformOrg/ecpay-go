f66a7a5955f2faa40ff0334c103e25e2a8edd720
Koopa <koopapapa@gmail.com>
2024-05-18 15:46:03 +0800

    log

--- pkg/logistics/business.go
+++ pkg/logistics/business.go
@@ -50,7 +50,7 @@ func (e *ECPayLogistics) CreateTestData() (*ECPayLogistics, error) {
 	}
 
 	slog.Info(fmt.Sprintf("responseData.TransCode %d", responseData.TransCode))
-	slog.Info(fmt.Sprintf("responseData.TransMsg %d", responseData.TransMsg))
+	slog.Info(fmt.Sprintf("responseData.TransMsg %s", responseData.TransMsg))
 	decryptedData := &ECPayLogistics{}
 	decryptedDataString, err := helpers.DecryptData(responseData.Data, e.Client.HashKey, e.Client.HashIV)
 	if err = json.NewDecoder(bytes.NewReader([]byte(decryptedDataString))).Decode(&decryptedData); err != nil {
